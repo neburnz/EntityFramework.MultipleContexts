5bfb4d98a5500dbb19e27ede5a19966afb5dfd7d
Ruben Guzman <neburnz@hotmail.com>
2014-11-07 10:01:49 -0600

    Added database and class library projects

---
+++ MainDatabase/Tables/first/Countries.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [first].[Countries]
+(
+	[CountryID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Code] VARCHAR(4) NULL, 
+    [CreationDate] DATETIME NULL, 
+    [Order] INT NULL
+)
---
+++ MainDatabase/Tables/first/States.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [first].[States]
+(
+	[StateID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [CountryID] INT NOT NULL, 
+    [Budget] MONEY NULL, 
+    CONSTRAINT [FK_States_Countries] FOREIGN KEY ([CountryID]) REFERENCES [first].[Countries]([CountryID])
+)
