3cbe6c4c1d26c2f9e463104967fad5534540b69a
Ruben Guzman <neburnz@hotmail.com>
2014-11-07 16:28:53 -0600

    Added additional tables

---
+++ MainDatabase/Tables/second/Addresses.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [second].[Addresses]
+(
+	[AddressID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StreetName] VARCHAR(50) NOT NULL, 
+    [StreetNumber] SMALLINT NULL, 
+    [PersonID] INT NULL, 
+    CONSTRAINT [FK_Addresses_People] FOREIGN KEY ([PersonID]) REFERENCES [second].[People]([PersonID])
+)
---
+++ MainDatabase/Tables/second/People.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [second].[People]
+(
+	[PersonID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Married] BIT NULL
+)
