355f55b7198e9933f72158615e35ef4966ac72b8
Ruben Guzman <neburnz@hotmail.com>
2014-11-07 14:21:07 -0600

    Added console application wit first context integrated

---
+++ MainConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstClassLibrary.Domain;
+
+namespace MainConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecting countries");
+            List<Country> countries = SelectCountries();
+
+            Console.WriteLine("Press intro key to continue...");
+            Console.Read();
+        }
+
+        private static List<Country> SelectCountries()
+        {
+            List<Country> result = null;
+
+            using (var db = new FirstModel())
+            {
+                List<Country> countries = db.Countries.ToList();
+            }
+
+            return result;
+        }
+    }
+}
