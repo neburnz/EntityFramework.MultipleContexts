3c8bdec0fa3bd993f1000dc418ff615031802211
Ruben Guzman <neburnz@hotmail.com>
2014-11-08 07:50:16 -0600

    Refactored contexts

---
+++ FirstClassLibrary/Domain/Extensions/FirstModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassLibrary.Domain
+{
+    public partial class FirstModel : DbContext
+    {
+        public FirstModel(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+    }
+}
---
+++ SecondClassLibrary/Domain/Extensions/SecondModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondClassLibrary.Domain
+{
+    public partial class SecondModel : DbContext
+    {
+        public SecondModel(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+    }
+}
